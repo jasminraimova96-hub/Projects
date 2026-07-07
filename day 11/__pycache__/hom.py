aa2ff00c922aad2f9608fbd27e8508d85645bf56
00016817wiut <145856019+00016817wiut@users.noreply.github.com>
2026-07-07 21:09:40 +0500

    remove pycache

--- day 11/__pycache__/hom.py
+++
@@ -1,9 +0,0 @@
-import json
-name = input("Enter name: ")
-age = input("Enter age: ")
-data = {"name": name, "age": age}
-with open("user.json", "w") as f:
-    json.dump(data, f)
-with open("user.json", "r") as f:
-    saved = json.load(f)
-print("Saved data:", saved)
